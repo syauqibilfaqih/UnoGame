070e4b9660d4336169604ffce1b32c0d99170183
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2023-12-26 13:43:03 +0700

    Added implementation to UnitTest1

--- UnoGame.Test/UnitTest1.cs
+++ UnoGame.Test/UnitTest1.cs
@@ -1,15 +1,34 @@
 namespace UnoGame.Test;
 
+[TestFixture]
 public class Tests
 {
+    private UnoGameController unoGameMaster;
+
     [SetUp]
     public void Setup()
     {
+        unoGameMaster = new UnoGameController();
     }
 
     [Test]
-    public void Test1()
+    public void Get_ReturnCorrectDirection_SwitchAndGetGameDirection()
     {
-        Assert.Pass();
+        Direction desiredDirection = Direction.CounterClockwise;
+        unoGameMaster.SwitchGameDirection(desiredDirection);
+        Direction savedDirection = unoGameMaster.GetGameDirection();
+        Assert.AreEqual(desiredDirection, savedDirection);
+    }
+
+    [TestCase(Color.Green)]
+	[TestCase(Color.Blue)]
+    [TestCase(Color.Red)]
+
+    public void Get_ReturnCorrectColor_PickAndGetPickedColor(Color c)
+    {
+        Color pickedColor = c;
+        unoGameMaster.PlayerPickColor(Color.Blue);
+        Color actualColor = unoGameMaster.GetPickedColor();
+        Assert.AreEqual(pickedColor, actualColor);
     }
 }
